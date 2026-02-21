b5701713be07e04d23142469c93f7840bacacb7f
Fasctor01729 <luo756feng@gmail.com>
2026-02-21 09:41:01 +0530

    commit

---
+++ Day21/Q42.c
@@ -0,0 +1,54 @@
+/* Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node. */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* findMiddle(struct Node* head) {
+    struct Node* slow = head;
+    struct Node* fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return slow;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    struct Node *head = NULL, *temp = NULL, *newNode;
+
+    for(int i = 0; i < n; i++) {
+        int value;
+        scanf("%d", &value);
+
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = value;
+        newNode->next = NULL;
+
+        if(head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    struct Node* middle = findMiddle(head);
+
+    if(middle != NULL)
+        printf("%d", middle->data);
+
+    return 0;
+}
